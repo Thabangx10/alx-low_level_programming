40cc2363ae423994bea2f975e3bf4473cf4b3be4
Thabangx10 <Thabangx10@gmail.com>
2022-09-21 19:54:08 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in reverse.
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+puchar('/n');
+
+return (0);
+}
