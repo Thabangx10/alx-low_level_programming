cafe7a0a5f76389e935e73b6b3bbbfc2512f8440
Thabangx10 <Thabangx10@gmail.com>
2022-12-10 10:36:52 -0800

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,15 +5,23 @@
  * @head: head
  * Return: the amount of nodes
  */
-size_t print_dlistint(const dlistint *h)
+size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h ->n);
-		h = h->next;
+		printf("%d\n", h->n);
 		count++;
+		h = h->next;
 	}
 
 	return (count);
