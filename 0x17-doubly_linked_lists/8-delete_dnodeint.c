df906c5e44317b34b15d46f7113978f46ea1bb91
Thabangx10 <Thabangx10@gmail.com>
2022-12-12 03:46:22 -0800

    8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * function: delete node at index
+ * @h: ** to the head
+ * @index: delete
+ * Return: 1,for list with deleted node and -1 on failing to.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *temp = NULL;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	temp = *head;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	while (temp->next != NULL)
+	{
+		if (count == index)
+		{
+			temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			free(temp);
+			return (1);
+		}
+		temp = temp->next;
+		count++;
+	}
+
+	return (-1);
+}
