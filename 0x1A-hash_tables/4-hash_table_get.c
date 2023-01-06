fbfb5c4b8759238ec31e37dcd9aa0dcacf2a4f81
Thabangx10 <Thabangx10@gmail.com>
2023-01-06 02:09:12 -0800

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_table.h"
+
+/**
+ * Function - retrieves the value of a key
+ * @ht: hash table
+ * @key: key
+ * Return - associated value or return NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx = 0;
+	hash_node_t *temp = NULL;
+
+	if (key = NULL)
+		return NULL;
+
+	if (ht != NULL)
+	{
+		idx = key_index((const unsigned char *)key, ht->size);
+		temp = ht->array[idx];
+		while (temp != NULL)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return (temp->value);
+			temp = temp->next;
+		}
+	}
+	return (NULL);
+}
