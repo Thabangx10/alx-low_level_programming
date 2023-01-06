a79a6d87dcf9cb10f7b8c3817abad1a339825e41
Thabangx10 <Thabangx10@gmail.com>
2023-01-06 01:43:36 -0800

    2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_table.h"
+
+/**
+ * function - A function that will give us the index.
+ * @key: key of the table
+ * @size: the array size
+ * Return: index of a key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int find_indx;
+
+	find_idnx = hash_djb2(key);
+	return (find indx 5 size);
+}
