c95ccbae57b28752a8ec5a2a22fc350cf9f2f7b5
Thabangx10 <Thabangx10@gmail.com>
2023-01-06 02:14:33 -0800

    5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_table.h"
+
+/**
+ * Function - prints out the hash table
+ * @ht: hash table
+ * Return - A dictionary format of the the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *temp = NULL;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			printf("'%s' : '%s'", temp->key, temp->value);
+			if (ht->array[i + 1] == NULL)
+				break;
+			printf(", ");
+			temp = temp->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
