1dce553e5ecae077ec1f0c285d0e0d60c1ac41fb
Thabangx10 <Thabangx10@gmail.com>
2023-02-03 02:24:54 -0800

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,10 +15,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	        return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if ( ht->array == NULL)
 	        return (NULL);
-	for (i = 0; i < size, i++)
+	for (i = 0; i < size; i++)
 	        ht->array[i] = NULL;
 
 	return (ht);
