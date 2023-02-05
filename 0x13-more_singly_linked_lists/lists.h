180bc33c40677b6dff8f670d038ad9e63a40fd10
Thabangx10 <Thabangx10@gmail.com>
2023-02-05 03:23:36 -0800

    0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,7 +15,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 /**
  * struct listp_s - singly linked list
@@ -28,7 +28,7 @@ typedef struct listp_s
 {
 	void *p;
 	struct listp_s *next;
-}listp_t;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
